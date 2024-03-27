7d0ba311f6b5900d5cfe83ec76ca59a4ffe3a62a
albertomola <pacmonkeykong@gmail.com>
2024-03-27 07:30:26 -0700

    Initial Commit

---
+++ Property Wrappers/Property_WrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Property_WrappersApp.swift
+//  Property Wrappers
+//
+//  Created by alberto on 27/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Property_WrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Property WrappersUITests/Property_WrappersUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Property_WrappersUITestsLaunchTests.swift
+//  Property WrappersUITests
+//
+//  Created by alberto on 27/3/24.
+//
+
+import XCTest
+
+class Property_WrappersUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
